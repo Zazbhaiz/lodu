bc3d291d19241bc557840f5dd0f45e491dcd542e
Zazbhai <96649732+Zazbhaiz@users.noreply.github.com>
2022-11-22 09:43:52 +0530

    Update start.py

--- YukkiMusic/plugins/techzbots/start.py
+++ YukkiMusic/plugins/techzbots/start.py
@@ -36,7 +36,7 @@ async def commands_menu(_, query):
 @app.on_callback_query(filters.regex("admin_cmd"))
 async def admin_menu(_, query):
     await query.answer()    
-    await query.message.edit(text=ADMIN_TEXT,reply_markup=BACK_BUTTON,parse_mode="markdown")
+    await query.message.edit(text=ADMIN_TEXT,reply_markup=ZAZ,parse_mode="markdown")
 
 @app.on_callback_query(filters.regex("auth_cmds"))
 async def auth_cmds(_, query):
